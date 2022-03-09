7fceade6288f04b9a6367085d383063bb8768409
unknown <kaithwas.697@gmail.com>
2022-03-09 15:24:40 +0530

    Added feature for Player to roll a dice

--- SnakesAndLadder/SnakesAndLadder/Program.cs
+++ SnakesAndLadder/SnakesAndLadder/Program.cs
@@ -4,10 +4,15 @@ namespace SnakesAndLadder
 {
     internal class Program
     {
+        public static int RollDice()
+        {
+            return new Random().Next(1,7);
+        }
         static void Main(string[] args)
         {
             int position = 0;
-            Console.WriteLine("Player is at {0}",position);
+            Console.WriteLine("Player is at {0}", position);
+            Console.WriteLine("Player gets: " + RollDice());
         }
     }
 }
